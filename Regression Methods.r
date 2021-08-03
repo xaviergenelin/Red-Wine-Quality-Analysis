191df0fe47c2dfc0ed2e25303945709d9062f3a9
Xavier <xgenelin@gmail.com>
2021-08-03 09:23:02 -0500

    Update Regression Methods.r

--- Regression Methods.r
+++ Regression Methods.r
@@ -1,5 +1,3 @@
-# Regressions
-
 ## OLS
 
 # Interaction
@@ -7,8 +5,50 @@
 # Polynomials
 
 # Lasso
+library(glmnet)
+grid<-10^seq(10,-2,length=100)
+
+xTrain <- model.matrix(quality ~ ., data = wineTrain)[, -1]
+yTrain <- wineTrain$quality
+
+lasso <- glmnet(xTrain, yTrain, alpha = 1, lambda = grid) 
+
+set.seed(13)
+cvLasso <- cv.glmnet(xTrain, yTrain, alpha = 1, nfolds = 11)
+
+bestLambda <- cvLasso$lambda.min
+
+bestLambda
+
+xTest <- model.matrix(quality ~ ., data = wineTest)[, -1]
+lassoPred <- predict(lasso, s = bestLambda, newx = xTest)
+
+mean((lassoPred - wineTest$quality)^2)
+
+lassoCoef <- predict(lasso, type = "coefficients", s = bestLambda)
+lassoCoef
 
 # Ridge
+grid <- 10^seq(10, -2, length = 100)
+
+xTrain <- model.matrix(quality ~ ., data = wineTrain)[, -1]
+yTrain <- wineTrain$quality
+
+ridge <- glmnet(xTrain, yTrain, alpha = 0, lambda = grid)
+
+set.seed(13)
+
+cvRidge <- cv.glmnet(xTrain, yTrain, alpha = 0, nfolds = 11)
+
+bestLambda <- cvRidge$lambda.min
+bestLambda
+
+ridgePred <- predict(ridge, s = bestLambda, newx = model.matrix(quality ~ ., data = wineTest)[, -1])
+
+yTest <- wineTest$quality
+
+mean((yTest - ridgePred)^2)
+
 
 # Decision Tree
  
